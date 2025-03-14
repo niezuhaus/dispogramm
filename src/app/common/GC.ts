84729ea884990da34597ac4a9c5437b432887ccd
niezuhaus <a@niezuhaus.de>
2025-03-14 19:12:32 +0100

    streetrouting per default off

--- src/app/common/GC.ts
+++ src/app/common/GC.ts
@@ -57,7 +57,7 @@ export class GC {
   public static backendIP = getItem<string>('backendIP') || GC.fallBackend;
   public static authName: string = getItem<string>('apiAuthName') || config.apiAuthName;
   public static authPwd: string = getItem<string>('apiAuthPwd') || config.apiAuthPwd;
-  public static streetRouting: boolean = getItem<boolean>('streetRouting') || true;
+  public static streetRouting: boolean = getItem<boolean>('streetRouting') || false;
   public static recentBackendIPs: string[] = [];
   public static cantConnect = false;
   public static apiKeyMissing = false;
