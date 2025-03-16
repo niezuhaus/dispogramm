c1087fcade1f41d705a3cf0d06cee86e4db5ffbd
niezuhaus <a@niezuhaus.de>
2025-03-16 16:01:43 +0100

    shifts overwies

--- src/app/views/shifts-overview/shifts-overwiew.component.ts
+++ src/app/views/shifts-overview/shifts-overwiew.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { TitleComponent } from '../app.component';
+import { GC } from 'src/app/common/GC';
+import { Messenger } from 'src/app/classes/Messenger';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-shifts-overwiew',
   templateUrl: './shifts-overwiew.component.html',
   styleUrls: ['./shifts-overwiew.component.scss']
 })
-export class ShiftsOverwiewComponent {
+export class ShiftsOverwiewComponent extends TitleComponent implements AfterViewInit {
 
+  override title = 'schichten';
+
+  date = new Date;
+
+  messengers = () => {
+    return GC.messengers.sort((a, b) => a.lastName?.localeCompare(b.lastName));
+  };
+
+  months = () => {
+    return GC.months;
+  }
+
+  ngAfterViewInit(): void {
+    console.log('loading now');
+    
+    GC.loadedCompletely.subscribe(() => {
+      this.messengers().forEach(m => m.loadShifts(this.date));
+    })
+  }
+
+  openDialog(messenger: Messenger): void {
+    new Messenger(messenger).openDialog();
+  }
+
+  monthChanged(month: Date): void {
+    GC.messengers.forEach(m => m.loadShifts(month));
+  }
 }
