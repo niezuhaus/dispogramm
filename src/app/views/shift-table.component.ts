512a721142992fd4c389996ccf5fb1029d2bb4df
niezuhaus <a@niezuhaus.de>
2025-03-25 17:03:10 +0100

    shift numbers begin at 1

--- src/app/views/shift-table.component.ts
+++ src/app/views/shift-table.component.ts
@@ -10,102 +10,107 @@ import { Messenger } from '../classes/Messenger';
 @Component({
   selector: 'shift-table',
   template: `
-            <table
-              #table
-              mat-table
-              [dataSource]="dataSource"
-              style="max-width: 100%;">
-              <ng-container matColumnDef="number">
-                <th mat-header-cell *matHeaderCellDef class="text-center" style="padding: unset; width: 50px">#</th>
-                <td mat-cell *matCellDef="let element; let i = index" class="text-center">
-                  {{i}}
-                </td>
-              </ng-container>
-              <ng-container matColumnDef="date">
-                <th mat-header-cell *matHeaderCellDef class="text-center">datum</th>
-                <td mat-cell *matCellDef="let element" style="width: 240px">
-                  <div class="flex flex-row justify-content-evenly align-items-end w-100" style="width: fit-content">
-                    <a *ngIf="!element.edit" (click)="openShiftDialog(element)" 
-                       >
-                      <p class="text-center noMargin">
-                        {{element.start.dateStampShort()}}, {{shiftLiterals(element.type)}}
-                      </p>
-                    </a>
+  <div>
+    <table
+      #table
+      mat-table
+      [dataSource]="dataSource"
+      style="max-width: 100%;">
+      <ng-container matColumnDef="number">
+        <th mat-header-cell *matHeaderCellDef class="text-center" style="padding: unset; width: 50px">#</th>
+        <td mat-cell *matCellDef="let element; let i = index" class="text-center">
+          {{i + 1}}
+        </td>
+      </ng-container>
+      <ng-container matColumnDef="date">
+        <th mat-header-cell *matHeaderCellDef class="text-center">datum</th>
+        <td mat-cell *matCellDef="let element" style="width: 240px">
+          <div class="flex flex-row justify-content-evenly align-items-end w-100" style="width: fit-content">
+            <a *ngIf="!element.edit" (click)="openShiftDialog(element)" 
+               >
+              <p class="text-center noMargin">
+                {{element.start.dateStampShort()}}, {{shiftLiterals(element.type)}}
+              </p>
+            </a>
 
 
-                    <mat-form-field *ngIf="element.edit" style="width: 100px" class="relative-top8px">
-                      <mat-label>datum</mat-label>
-                      <div class="flex flex-row">
-                        <input matInput [matDatepicker]="picker" [(ngModel)]="element.start"
-                               (keydown)="$event.key === 'Enter' ? updateShift(element) : ''">
-                        <i class="bi bi-calendar3" (click)="picker.open()"></i>
-                      </div>
-                      <mat-datepicker #picker></mat-datepicker>
-                    </mat-form-field>
+            <mat-form-field *ngIf="element.edit" style="width: 100px" class="relative-top8px">
+              <mat-label>datum</mat-label>
+              <div class="flex flex-row">
+                <input matInput [matDatepicker]="picker" [(ngModel)]="element.start"
+                       (keydown)="$event.key === 'Enter' ? updateShift(element) : ''">
+                <i class="bi bi-calendar3" (click)="picker.open()"></i>
+              </div>
+              <mat-datepicker #picker></mat-datepicker>
+            </mat-form-field>
 
-                    <mat-form-field class="ml-3 relative-top8px" *ngIf="element.edit"
-                                    style="width: 100px">
-                      <mat-label>schichttyp</mat-label>
-                      <mat-select
-                        tabindex="-1"
-                        #type
-                        [(ngModel)]="element.type"
-                        (selectionChange)="element.startTimeGuess(); element.endTimeGuess()"
-                        (keydown)="$event.key === 'Enter' ? updateShift(element) : ''">
-                        <mat-option
-                          *ngFor="let shiftType of messengerShiftTypes; let i = index"
-                          [value]="i">
-                          {{shiftType}}
-                        </mat-option>
-                      </mat-select>
-                    </mat-form-field>
+            <mat-form-field class="ml-3 relative-top8px" *ngIf="element.edit"
+                            style="width: 100px">
+              <mat-label>schichttyp</mat-label>
+              <mat-select
+                tabindex="-1"
+                #type
+                [(ngModel)]="element.type"
+                (selectionChange)="element.startTimeGuess(); element.endTimeGuess()"
+                (keydown)="$event.key === 'Enter' ? updateShift(element) : ''">
+                <mat-option
+                  *ngFor="let shiftType of messengerShiftTypes; let i = index"
+                  [value]="i">
+                  {{shiftType}}
+                </mat-option>
+              </mat-select>
+            </mat-form-field>
 
-                  </div>
-                </td>
-              </ng-container>
-              <ng-container matColumnDef="startend">
-                <th mat-header-cell *matHeaderCellDef class="text-center" style="min-width: 80px">arbeitszeit</th>
-                <td mat-cell *matCellDef="let element" style="width: 260px; padding: 0 20px !important;">
-                  <div *ngIf="element.edit" class="flex flex-row align-items-center justify-content-between">
-                    <timepicker
-                      [label]="'start'"
-                      [(time)]="element.start"
-                      (keydown)="$event.key === 'Enter' ? updateShift(element) : ''"
-                      class="relative-top8px"></timepicker>
-                    <span class="mx-2">-</span>
-                    <timepicker
-                      [label]="'ende'"
-                      [(time)]="element.end" [compareDate]="element.start"
-                      (keydown)="$event.key === 'Enter' ? updateShift(element) : ''"
-                      class="relative-top8px"></timepicker>
-                  </div>
-                  <div *ngIf="!element.edit" class="flex flex-row align-items-center justify-content-between">
-                    <span>{{element.start.timestamp()}}</span>
-                    <span class="mx-2">-</span>
-                    <span *ngIf="element.end">{{element.end?.timestamp()}}</span>
-                    <a *ngIf="!element.end" class="fex-warn" (click)="element.edit = true">endzeit eintragen</a>
-                  </div>
+          </div>
+        </td>
+      </ng-container>
+      <ng-container matColumnDef="startend">
+        <th mat-header-cell *matHeaderCellDef class="text-center" style="min-width: 80px">arbeitszeit</th>
+        <td mat-cell *matCellDef="let element" style="width: 260px; padding: 0 20px !important;">
+          <div *ngIf="element.edit" class="flex flex-row align-items-center justify-content-between">
+            <timepicker
+              [label]="'start'"
+              [(time)]="element.start"
+              (keydown)="$event.key === 'Enter' ? updateShift(element) : ''"
+              class="relative-top8px"></timepicker>
+            <span class="mx-2">-</span>
+            <timepicker
+              [label]="'ende'"
+              [(time)]="element.end" [compareDate]="element.start"
+              (keydown)="$event.key === 'Enter' ? updateShift(element) : ''"
+              class="relative-top8px"></timepicker>
+          </div>
+          <div *ngIf="!element.edit" class="flex flex-row align-items-center justify-content-between">
+            <span>{{element.start.timestamp()}}</span>
+            <span class="mx-2">-</span>
+            <span *ngIf="element.end">{{element.end?.timestamp()}}</span>
+            <a *ngIf="!element.end" class="fex-warn" (click)="element.edit = true">endzeit eintragen</a>
+          </div>
 
-                </td>
-              </ng-container>
-              <ng-container matColumnDef="money">
-                <th mat-header-cell *matHeaderCellDef class="text-center"></th>
-                <td mat-cell *matCellDef="let element">
-                  <p class="text-center noMargin" *ngIf="element.shiftType <= 4 && !element.edit">
-                    {{element.money.netto}}
-                  </p>
-                  <div *ngIf="element.edit" class="flex justify-content-around">
-                    <button mat-button class="align-items-center"
-                            (click)="updateShift(element)">
-                      <i class="bi bi-check"></i>
-                    </button>
-                  </div>
-                </td>
-              </ng-container>
-              <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-              <tr mat-row *matRowDef="let row; columns: displayedColumns;"
-                  (contextmenu)="onRightClick($event, row)"></tr>
-            </table>
+        </td>
+      </ng-container>
+      <ng-container matColumnDef="money">
+        <th mat-header-cell *matHeaderCellDef class="text-center"></th>
+        <td mat-cell *matCellDef="let element">
+          <p class="text-center noMargin" *ngIf="element.shiftType <= 4 && !element.edit">
+            {{element.money.netto}}
+          </p>
+          <div *ngIf="element.edit" class="flex justify-content-around">
+            <button mat-button class="align-items-center"
+                    (click)="updateShift(element)">
+              <i class="bi bi-check"></i>
+            </button>
+          </div>
+        </td>
+      </ng-container>
+      <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+      <tr mat-row *matRowDef="let row; columns: displayedColumns;"
+          (contextmenu)="onRightClick($event, row)"></tr>
+    </table>
+  </div>
+  <div *ngIf="!shifts" class="flex justify-content-center align-items-center" style="height: 20vh">
+            <h3>- keine schichten gefunden -</h3>
+          </div>
 
             <div class="container">
               <div style="visibility: hidden; position: fixed"
