e3af775a2f0ae6caf839e0da9a51058b47936fa1
niezuhaus <a@niezuhaus.de>
2025-04-08 23:05:03 +0200

    checkboy messenger filter

--- src/app/views/shifts-overview/shifts-overwiew.component.ts
+++ src/app/views/shifts-overview/shifts-overwiew.component.ts
@@ -11,10 +11,13 @@ import { ActivatedRoute } from '@angular/router';
   template: `
   <div class="h-100">
     <div class="flex w-100 pt-3 p-4 flex-column justify-content-between" style="background-color: white; z-index: 1;">
+      <div class="flex flex-row w-100 align-items-center">
         <datepicker [(date)]="date" [monthly]="true" (dateChange)="monthChanged($event)" #datepicker>
         </datepicker>
+        <mat-checkbox [checked]="filtered" (change)="toggleFilter()">nur kurier:innen mit schicht</mat-checkbox>
+      </div>
 
-          <div *ngFor="let m of messengers()">
+          <div *ngFor="let m of hideShiftless ? filteredMessenger : messengers">
               <div [style.opacity]="m.shifts.length ? 1 : .25">
                   <h3 style="cursor: pointer; white-space: nowrap; margin: 0">
                       <a (click)="m.openDialog(true)">{{!m.lastName ? '(kein nachname)' : m.lastName}}, {{!m.firstName ?
@@ -36,18 +39,16 @@ import { ActivatedRoute } from '@angular/router';
   `,
   styleUrls: ['./shifts-overwiew.component.scss']
 })
-export class ShiftsOverwiewComponent extends TitleComponent implements AfterViewInit {
+export class ShiftsOverwiewComponent extends TitleComponent implements OnInit, AfterViewInit {
 
   override title = 'schichten';
   shiftsLoaded = false;
 
   date = new Date;
+  filtered = true;
 
-  messengers = () => {
-    return GC.messengers.sort((a, b) => {
-      return (a.lastName || 'ß').localeCompare(b.lastName || 'ß');
-    });
-  };
+  messengers: Messenger[] = [];
+  filteredMessenger: Messenger[] = [];
 
   months = () => {
     return GC.months;
@@ -56,6 +57,7 @@ export class ShiftsOverwiewComponent extends TitleComponent implements AfterView
   minimumWage = () => {
     return GC.config.minimumWage;
   }
+  get hideShiftless() {return GC.config.shifts.hideShiftless};
 
   @ViewChild('table') table: ShiftTableComponent;
 
@@ -67,21 +69,40 @@ export class ShiftsOverwiewComponent extends TitleComponent implements AfterView
     super();
   }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       if (params.get('month')) {
-        this.date.setDateByString(params.get('month' + '-01'));
+        this.date.setDateByString(params.get('month') + '-01');
       } else {
         GC.location.replaceState(`${GC.routes.shifts};month=${this.date.yyyymm()}`);
       }
     });
+  }
+
+  ngAfterViewInit(): void {
     GC.loaded().subscribe(() => {
+      this.messengers = GC.messengers.sort((a, b) => {
+        switch (true) {
+          case !a.lastName && !b.lastName:
+            return 0;
+  
+          case !a.lastName:
+            return 1;
+  
+          case !b.lastName:
+            return -1;
+        }
+  
+        return (a.lastName).localeCompare(b.lastName);
+      });
+
       this.load();
     })
   }
 
   load(): void {
-    zip(this.messengers().map(m => m.loadShifts(this.date))).subscribe(() => {
+    zip(this.messengers.map(m => m.loadShifts(this.date))).subscribe(res => {
+      this.filteredMessenger = this.messengers.filter((m, i) => res[i].length);
       this.shiftsLoaded = true;
       this.cd.detectChanges();
     })
@@ -91,5 +112,12 @@ export class ShiftsOverwiewComponent extends TitleComponent implements AfterView
   monthChanged(month: Date): void {
     this.load();
     GC.location.replaceState(`${GC.routes.shifts};month=${this.date.yyyymm()}`);
+  }
+
+  toggleFilter(): void {
+    GC.config.shifts.hideShiftless = !GC.config.shifts.hideShiftless;
+    GC.http.saveConfigItem('hideInactive', GC.config.shifts.hideShiftless.toString()).subscribe(() => {
+      GC.openSnackBarShort(`kurier:innen ohne schicht ${GC.config.shifts.hideShiftless ? 'ausgeblendet' : 'eingeblendet'}`);
+    });
   }
 }
