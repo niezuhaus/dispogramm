f731953532549beab1a04ac11f0917d2c29c00ad
niezuhaus <53942131+niezuhaus@users.noreply.github.com>
2025-04-18 01:40:41 +0200

    input self hosted backend

--- e2e/backend.setup.ts
+++ e2e/backend.setup.ts
@@ -4,7 +4,7 @@ setup('backend_setup', async ({ page }) => {
     await page.goto('http://localhost:4200/');
     await page.locator('a').click();
     await page.locator('#mat-input-0').press('ControlOrMeta+a');
-    await page.locator('#mat-input-0').fill('http://localhost:8081/');
+    await page.locator('#mat-input-0').fill('https://cloud.niezuhaus.de/api/');
     await page.getByRole('button', { name: 'speichern' }).click();
     await page.context().storageState({ path: "e2e/env/storage.json" })
 });
