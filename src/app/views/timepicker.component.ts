12b0871138b7638efab0c9861d9f762fda4286ad
niezuhaus <53942131+niezuhaus@users.noreply.github.com>
2025-05-27 03:22:18 +0200

    should work better with tab

--- src/app/views/timepicker.component.ts
+++ src/app/views/timepicker.component.ts
@@ -13,12 +13,12 @@ import { debounceTime } from 'rxjs/operators';
           type="time"
           matInput
           [disabled]="disabled"
-          [value]="timestamp(time)"
-          (change)="setTime(input.value, 'change')"
-          (reset)="setTime('', 'reset')"
+          [value]="timestamp(_time)"
+          (change)="setTime(input.value)"
+          (reset)="setTime('')"
           (keyup)="keys = keys + 1"
           [style.width.px]="width"
-          tabindex="-1"
+          tabindex="0"
           #input
         />
       </mat-form-field>
@@ -43,31 +43,40 @@ export class TimepickerComponent implements OnInit {
   @Input() width: number;
   @Output() timeChange = new EventEmitter<Date>();
   @Output() timeChangeDebounce = new EventEmitter<Date>();
+  _time: Date;
   today = this.dateAdapter.today();
   keys = 0;
 
   constructor(private dateAdapter: DateAdapter<Date>) {}
 
   ngOnInit(): void {
+    this._time = this.time;
     this.timeChange.pipe(debounceTime(1000)).subscribe((date) => this.timeChangeDebounce.emit(date));
   }
 
-  setTime(s: string, action: string): void {
+  onTimeUpdate(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+
+    // this.setTime(input.value, 'timeupdate');
+  }
+
+  setTime(s: string): void {
     if (!s || s === '') {
       if (!this.disableAutoReplace) {
-        this.timeChange.emit(this.time);
+        this.timeChange.emit(this._time);
         return;
       }
-      this.time.setHours(this.today.getHours());
-      this.time.setMinutes(this.today.getMinutes());
+      this._time.setHours(this.today.getHours());
+      this._time.setMinutes(this.today.getMinutes());
     } else {
       const h = s.slice(0, 2);
       const m = s.slice(3, 5);
-      this.time = new Date(this.time);
-      this.time.setHours(parseInt(h));
-      this.time.setMinutes(parseInt(m));
+      this._time = new Date(this._time);
+      this._time.setHours(parseInt(h));
+      this._time.setMinutes(parseInt(m));
     }
-    this.timeChange.emit(this.time);
+    this.timeChange.emit(this._time);
   }
 
   timestamp(date: Date): string {
@@ -80,5 +89,17 @@ export class TimepickerComponent implements OnInit {
     }
     date = new Date(date);
     return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+  }
+
+  makeFiveHoursShift(date: Date): void {
+    const newDate = new Date(date);
+    newDate.setHours(newDate.getHours() + 5);
+
+    // Cap at 18:00:00
+    if (newDate.getHours() > 18 || (newDate.getHours() === 18 && (newDate.getMinutes() > 0 || newDate.getSeconds() > 0))) {
+      newDate.setHours(18, 0, 0, 0);
+    }
+
+    this.setTime(this.timestamp(newDate));
   }
 }
