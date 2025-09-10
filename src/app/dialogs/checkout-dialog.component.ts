8d7864e8e0a1397e4856767a705210f0046ef865
dani* <53942131+niezuhaus@users.noreply.github.com>
2025-09-10 20:44:47 +0200

    remove padding because of changes in material padding

--- src/app/dialogs/checkout-dialog.component.ts
+++ src/app/dialogs/checkout-dialog.component.ts
@@ -13,138 +13,168 @@ import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-checkout-dialog',
   template: `
-    <div style="min-width: 600px">
-      <div class="flex flex-row justify-content-between">
-        <div>
-          <h1 mat-dialog-title>{{ data.shift.messenger.nickname }}s schicht vom {{ data.shift.start.dateStampLong() }}</h1>
-          <p *ngIf="!finished">bitte schließe die touren ab, für die du die belege vorliegen hast</p>
-        </div>
-        <div class="flex justify-content-around ml-4" style="height: fit-content">
-          <div id="times" class="flex px-3 py-3 flex-column align-items-center">
-            <div class="flex flex-row justify-around w-100 align-items-center">
-              <timepicker [label]="'check-in'" [(time)]="data.shift.start" class="relative-top8px mr-5" [disabled]="finished"></timepicker>
-              <timepicker [label]="'check-out'" [(time)]="end" class="relative-top8px" [disabled]="finished"></timepicker>
+    <div class="p-4 overflow-hidden">
+      <div style="min-width: 600px">
+        <div class="flex flex-row justify-content-between">
+          <div>
+            <h1 mat-dialog-title>
+              {{ data.shift.messenger.nickname }}s {{ shiftLiterals[this.data.shift.type] }}schicht vom {{ data.shift.start.dateStampLong() }}
+            </h1>
+            <p *ngIf="!finished">bitte schließe die touren ab, für die du die belege vorliegen hast</p>
+          </div>
+          <div class="flex justify-content-around ml-4" style="height: fit-content">
+            <div id="times" class="flex px-3 py-3 flex-column align-items-center">
+              <div class="flex flex-row justify-around w-100 align-items-center">
+                <timepicker [label]="'check-in'" [(time)]="data.shift.start" class="relative-top8px mr-5" [disabled]="finished"></timepicker>
+                <timepicker [label]="'check-out'" [(time)]="end" class="relative-top8px" [disabled]="finished"></timepicker>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div style="max-height: 50vh; overflow-y: scroll; overflow-x: clip">
-      <p *ngIf="data.jobs.length === 0" class="text-center mt-5"><i>- keine touren vorhanden -</i></p>
-      <table
-        *ngIf="data.jobs.length > 0"
-        id="jobs"
-        [style.visibility]="data.jobs && data.jobs.length > 0 ? 'unset' : 'hidden'"
-        mat-table
-        [dataSource]="dataSource"
-        class="ml-3"
-        matSort
-        style="max-width: 1400px; min-width: 800px"
-        matSortActive="date"
-        matSortDirection="desc"
-      >
-        <ng-container matColumnDef="date">
-          <th mat-header-cell *matHeaderCellDef mat-sort-header style="width: 80px">zeit</th>
-          <td mat-cell *matCellDef="let element" class="text-center">
-            <span *ngIf="!element.edit">
-              {{ element.date.timestamp() }}
-            </span>
-            <timepicker [label]="'zeit'" *ngIf="element.edit" [(time)]="element.date" (keydown)="$event.key === 'Enter' ? element.update('auftrag wurde gespeichert.') : ''"></timepicker>
-          </td>
-        </ng-container>
-        <ng-container matColumnDef="description">
-          <th mat-header-cell *matHeaderCellDef mat-sort-header>beschreibung</th>
-          <td mat-cell *matCellDef="let element" style="text-align: left" class="mx-2">
-            <description [job]="element" class="p-0" [headline]="true" [hideHighlights]="true"></description>
-          </td>
-        </ng-container>
-        <ng-container matColumnDef="creator">
-          <th mat-header-cell *matHeaderCellDef style="width: 80px;" class="mx-2">angelegt von</th>
-          <td mat-cell *matCellDef="let element">
-            <span *ngIf="element.creator">{{ element.creator.nickname }}</span>
-          </td>
-        </ng-container>
-        <ng-container matColumnDef="price">
-          <th mat-header-cell *matHeaderCellDef mat-sort-header class="mx-2" style="width: 160px">preis</th>
-          <td mat-cell *matCellDef="let element" style="color: gray;" class="text-center">
-            <div *ngIf="!element.edit">
-              <span [style.color]="!element.showBrutto ? 'black' : '#BBB'">{{ element.price?._netto || element.price._netto }}</span>
-              / <span [style.color]="element.showBrutto ? 'black' : '#BBB'">{{ element.price?._brutto || element.price._brutto }}</span
-              ><br />
-              {{ element._waitingMinutes > 0 ? '(inkl. ' + element.waitingPrice?.netto + ' für ' + element._waitingMinutes + 'min)' : '' }}
-            </div>
+      <div style="max-height: 50vh; overflow-y: scroll; overflow-x: clip" *ngIf="this.data.shift.type >= 2">
+        <p *ngIf="data.jobs.length === 0" class="text-center mt-5"><i>- keine touren vorhanden -</i></p>
+        <table
+          *ngIf="data.jobs.length > 0"
+          id="jobs"
+          [style.visibility]="data.jobs && data.jobs.length > 0 ? 'unset' : 'hidden'"
+          mat-table
+          [dataSource]="dataSource"
+          class="ml-3"
+          matSort
+          style="max-width: 1400px; min-width: 800px"
+          matSortActive="date"
+          matSortDirection="desc"
+        >
+          <ng-container matColumnDef="date">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header style="width: 80px">zeit</th>
+            <td mat-cell *matCellDef="let element" class="text-center">
+              <span *ngIf="!element.edit">
+                {{ element.date.timestamp() }}
+              </span>
+              <timepicker
+                [label]="'zeit'"
+                *ngIf="element.edit"
+                [(time)]="element.date"
+                (keydown)="$event.key === 'Enter' ? element.update('auftrag wurde gespeichert.') : ''"
+              ></timepicker>
+            </td>
+          </ng-container>
+          <ng-container matColumnDef="description">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header>beschreibung</th>
+            <td mat-cell *matCellDef="let element" style="text-align: left" class="mx-2">
+              <description [job]="element" class="p-0" [headline]="true" [hideHighlights]="true"></description>
+            </td>
+          </ng-container>
+          <ng-container matColumnDef="creator">
+            <th mat-header-cell *matHeaderCellDef style="width: 80px;" class="mx-2">angelegt von</th>
+            <td mat-cell *matCellDef="let element">
+              <span *ngIf="element.creator">{{ element.creator.nickname }}</span>
+            </td>
+          </ng-container>
+          <ng-container matColumnDef="price">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header class="mx-2" style="width: 160px">preis</th>
+            <td mat-cell *matCellDef="let element" style="color: gray;" class="text-center">
+              <div *ngIf="!element.edit">
+                <span [style.color]="!element.showBrutto ? 'black' : '#BBB'">{{ element.price?._netto || element.price._netto }}</span>
+                / <span [style.color]="element.showBrutto ? 'black' : '#BBB'">{{ element.price?._brutto || element.price._brutto }}</span
+                ><br />
+                {{ element._waitingMinutes > 0 ? '(inkl. ' + element.waitingPrice?.netto + ' für ' + element._waitingMinutes + 'min)' : '' }}
+              </div>
 
-            <div *ngIf="element.edit" class="flex flex-row align-items-center">
-              <mat-form-field>
-                <mat-label>netto</mat-label>
-                <input
-                  #netto
-                  matInput
-                  (keyup)="changePrice(element.price, netto.value.valueOf(), false)"
-                  (keydown)="$event.key === 'Enter' ? element.update('auftrag wurde gespeichert.') : ''"
-                  (click)="netto.select()"
-                  [value]="element.price?._netto.toString()"
-                />
-              </mat-form-field>
-              <span class="mx-2">/</span>
-              <mat-form-field>
-                <mat-label>brutto</mat-label>
-                <input
-                  #brutto
-                  matInput
-                  (keyup)="changePrice(element.price, brutto.value.valueOf(), true)"
-                  (keydown)="$event.key === 'Enter' ? element.update('auftrag wurde gespeichert.') : ''"
-                  (click)="brutto.select()"
-                  [value]="element.price?._brutto.toString()"
-                />
-              </mat-form-field>
-            </div>
-          </td>
-        </ng-container>
-        <ng-container matColumnDef="waitingMinutes">
-          <th mat-header-cell *matHeaderCellDef style="width: 100px;" class="mx-2 text-center">wartezeit</th>
-          <td mat-cell *matCellDef="let element" style="color: gray;">
-            <div>
-              <mat-form-field>
-                <mat-label>minuten</mat-label>
-                <input type="number" #minutes (click)="minutes.select()" matInput name="waitingMinutes" [(ngModel)]="element._waitingMinutes" (change)="tourplan.calcSales()" />
-              </mat-form-field>
-            </div>
-          </td>
-        </ng-container>
-        <ng-container matColumnDef="finish">
-          <th mat-header-cell *matHeaderCellDef style="width: 200px;"></th>
-          <td mat-cell *matCellDef="let element">
-            <div class="flex flex-row justify-content-around">
-              <button *ngIf="!element.finished" mat-raised-button class="fex-button" (click)="element._finished = true; addToSum(element)">tour abschließen</button>
-              <button *ngIf="element.finished" mat-raised-button class="fex-button-abort" (click)="element._finished = false; subFromSum(element)" matTooltip="öffnen">
-                <i class="bi bi-check"></i> tour abgeschlossen
-              </button>
-            </div>
-          </td>
-        </ng-container>
-        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-        <tr mat-row *matRowDef="let row; columns: displayedColumns" (contextmenu)="onRightClick($event, row)"></tr>
-      </table>
-    </div>
+              <div *ngIf="element.edit" class="flex flex-row align-items-center">
+                <mat-form-field>
+                  <mat-label>netto</mat-label>
+                  <input
+                    #netto
+                    matInput
+                    (keyup)="changePrice(element.price, netto.value.valueOf(), false)"
+                    (keydown)="$event.key === 'Enter' ? element.update('auftrag wurde gespeichert.') : ''"
+                    (click)="netto.select()"
+                    [value]="element.price?._netto.toString()"
+                  />
+                </mat-form-field>
+                <span class="mx-2">/</span>
+                <mat-form-field>
+                  <mat-label>brutto</mat-label>
+                  <input
+                    #brutto
+                    matInput
+                    (keyup)="changePrice(element.price, brutto.value.valueOf(), true)"
+                    (keydown)="$event.key === 'Enter' ? element.update('auftrag wurde gespeichert.') : ''"
+                    (click)="brutto.select()"
+                    [value]="element.price?._brutto.toString()"
+                  />
+                </mat-form-field>
+              </div>
+            </td>
+          </ng-container>
+          <ng-container matColumnDef="waitingMinutes">
+            <th mat-header-cell *matHeaderCellDef style="width: 100px;" class="mx-2 text-center">wartezeit</th>
+            <td mat-cell *matCellDef="let element" style="color: gray;">
+              <div>
+                <mat-form-field>
+                  <mat-label>minuten</mat-label>
+                  <input
+                    type="number"
+                    #minutes
+                    (click)="minutes.select()"
+                    matInput
+                    name="waitingMinutes"
+                    [(ngModel)]="element._waitingMinutes"
+                    (change)="tourplan.calcSales()"
+                  />
+                </mat-form-field>
+              </div>
+            </td>
+          </ng-container>
+          <ng-container matColumnDef="finish">
+            <th mat-header-cell *matHeaderCellDef style="width: 200px;"></th>
+            <td mat-cell *matCellDef="let element">
+              <div class="flex flex-row justify-content-around">
+                <button *ngIf="!element.finished" mat-raised-button class="fex-button" (click)="element._finished = true; addToSum(element)">
+                  tour abschließen
+                </button>
+                <button
+                  *ngIf="element.finished"
+                  mat-raised-button
+                  class="fex-button-abort"
+                  (click)="element._finished = false; subFromSum(element)"
+                  matTooltip="öffnen"
+                >
+                  <i class="bi bi-check"></i> tour abgeschlossen
+                </button>
+              </div>
+            </td>
+          </ng-container>
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns" (contextmenu)="onRightClick($event, row)"></tr>
+        </table>
+      </div>
 
-    <div id="sum" class="mt-4 fex-dark" style="width: calc(100% + 48px); position: relative; left: -24px">
-      <h3 style="margin: unset">rechnung: {{ finishingBillSum._netto }}, bareinnahmen: {{ finishingCashSum._brutto }}</h3>
-      <p style="margin: unset">die bareinnahmen sind nicht im rechnungsbetrag enthalten.</p>
-    </div>
+      <div id="sum" class="mt-4 fex-dark" *ngIf="this.data.shift.type >= 2">
+        <h3 style="margin: unset">rechnung: {{ finishingBillSum._netto }}, bareinnahmen: {{ finishingCashSum._brutto }}</h3>
+        <p style="margin: unset">die bareinnahmen sind nicht im rechnungsbetrag enthalten.</p>
+      </div>
 
-    <div class="mt-4 flex flex-row justify-content-between align-items-end">
-      <button *ngIf="!finished" mat-raised-button class="fex-button" (click)="checkout()">
-        {{ data.shift.messenger.nickname }} für {{ end?.timestamp() || data.shift.end.timestamp() }} auschecken
-      </button>
-      <button *ngIf="finished" mat-raised-button class="fex-button" disabled>{{ data.shift.messenger.nickname }} wurde ausgecheckt</button>
+      <div class="mt-4 flex flex-row justify-content-between align-items-end">
+        <button *ngIf="!finished" mat-raised-button class="fex-button" (click)="checkout()">
+          {{ data.shift.messenger.nickname }} für {{ end?.timestamp() || data.shift.end.timestamp() }} auschecken
+        </button>
+        <button *ngIf="finished" mat-raised-button class="fex-button" disabled>{{ data.shift.messenger.nickname }} wurde ausgecheckt</button>
 
-      <button mat-raised-button class="fex-button fex-button-abort" matDialogClose>schließen</button>
+        <button mat-raised-button class="fex-button fex-button-abort" matDialogClose>schließen</button>
+      </div>
     </div>
 
     <div class="container">
-      <div style="visibility: hidden; position: fixed" [style.left.px]="menuTopLeftPosition.x" [style.top.px]="menuTopLeftPosition.y" [matMenuTriggerFor]="rightMenu"></div>
+      <div
+        style="visibility: hidden; position: fixed"
+        [style.left.px]="menuTopLeftPosition.x"
+        [style.top.px]="menuTopLeftPosition.y"
+        [matMenuTriggerFor]="rightMenu"
+      ></div>
       <mat-menu #rightMenu="matMenu">
         <ng-template matMenuContent let-item="item">
           <right-click-menu [job]="item"> </right-click-menu>
@@ -154,23 +184,23 @@ import { FormControl, Validators } from '@angular/forms';
   `,
   styles: [
     `
-          @import '../../const.scss';
-    
-          #sum {
-            width: calc(100% + 48px);
-            position: relative;
-            left: -24px;
-            color: white;
-            display: flex;
-            flex-direction: column;
-            text-align: center;
-          }
-    
-          #times {
-            // border: $fex-dark 4px solid;
-            // border-radius: 14px;
-          }
-        `
+      @import '../../const.scss';
+
+      #sum {
+        width: calc(100% + 60px);
+        position: relative;
+        left: -30px;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+      }
+
+      #times {
+        // border: $fex-dark 4px solid;
+        // border-radius: 14px;
+      }
+    `
   ]
 })
 export class CheckoutDialogComponent implements OnInit {
@@ -187,6 +217,9 @@ export class CheckoutDialogComponent implements OnInit {
 
   get tourplan() {
     return GC.tourplan;
+  }
+  get shiftLiterals() {
+    return GC.shiftLiterals;
   }
 
   expenses = 0;
--- src/app/dialogs/shifts-dialog/check-in-dialog.component.ts
+++ src/app/dialogs/shifts-dialog/check-in-dialog.component.ts
@@ -10,61 +10,70 @@ import { MatMenuTrigger } from '@angular/material/menu';
 @Component({
   selector: 'change-user-dialog',
   template: `
-    <h1 *ngIf="data?.morning" mat-dialog-title style="max-width: 400px;">guten morgen! wer ist heute am start?</h1>
-    <h1 *ngIf="!data?.morning" mat-dialog-title style="max-width: 400px;">wer ist in der schicht?</h1>
-    <div id="change-user-content" class="mt-2 justify-content-around" style="min-width: 400px">
-      <div class="flex flex-column justify-content-around">
-        <div class="p-4">
-          <new-shift-input [disabled]="!!dispatcher" [dispatcher]="true" (dispatcherShiftCreated)="dispatcherShift = $event"> </new-shift-input>
+    <mat-tab-group>
+      <mat-tab label="kurier:innen ein- und auschecken">
+        <h3 *ngIf="data?.morning" style="max-width: 400px;" class="pl-3">guten morgen! wer ist heute am start?</h3>
+        <h3 *ngIf="!data?.morning" style="max-width: 400px;" class="pl-3">wer ist in der schicht?</h3>
+        <div id="change-user-content" class="mt-2 justify-content-around" style="min-width: 400px">
+          <div class="flex flex-column justify-content-around">
+            <div class="p-4">
+              <new-shift-input [disabled]="!!dispatcher" [dispatcher]="true" (dispatcherShiftCreated)="dispatcherShift = $event"> </new-shift-input>
 
-          <new-shift-input (messengerShiftCreated)="messengerShifts.push($event)" #messengerInput [ignoredMessenger]="messengerToday"> </new-shift-input>
+              <new-shift-input (messengerShiftCreated)="messengerShifts.push($event)" #messengerInput [ignoredMessenger]="messengerToday"> </new-shift-input>
+            </div>
+            <div class="overflow-y-scroll" style="max-height: 60vh">
+              <div *ngIf="dispatcherShift !== null || messengerShifts.length">
+                <div class="mb-3 border-top border-bottom shiftBlock">
+                  <h3 class="my-3">ausgewählte kurier:innen zum einchecken</h3>
+                  <shift
+                    *ngFor="let shift of (dispatcherShift ? [dispatcherShift] : []).concat(messengerShifts)"
+                    class="flex flex-row pt-3 align-items-center w-100 border-bottom"
+                    [shift]="shift"
+                    (deleteFromCheckIn)="deleteFromCheckin($event)"
+                  >
+                  </shift>
+                </div>
+
+                <button mat-raised-button class="m-4 fex-button" (click)="checkIn()" matDialogClose>jetzt einchecken</button>
+              </div>
+
+              <div *ngIf="openShiftsToday.length" class="pb-3 border-top border-bottom shiftBlock">
+                <h3 class="my-3">eingecheckte kurier:innen</h3>
+                <div style="min-height: 100px">
+                  <shift
+                    *ngFor="let shift of openShiftsToday"
+                    class="flex flex-row pt-3 align-items-center justify-content-between w-100 border-bottom"
+                    [shift]="shift"
+                    (contextmenu)="onRightClick($event, shift)"
+                  >
+                  </shift>
+                </div>
+              </div>
+
+              <div *ngIf="closedShiftsToday.length" class="border-top border-bottom shiftBlock">
+                <h3 class="my-3">beendete schichten</h3>
+                <div style="min-height: 100px">
+                  <shift
+                    *ngFor="let shift of closedShiftsToday"
+                    class="flex flex-row pt-3 align-items-center justify-content-between w-100 border-bottom"
+                    [shift]="shift"
+                    (contextmenu)="onRightClick($event, shift)"
+                  >
+                  </shift>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="overflow-y-scroll" style="max-height: 60vh">
-          <div *ngIf="dispatcherShift !== null || messengerShifts.length">
-            <div class="mb-3 border-top border-bottom shiftBlock">
-              <h3 class="my-3">ausgewählte kurier:innen zum einchecken</h3>
-              <shift
-                *ngFor="let shift of (dispatcherShift ? [dispatcherShift] : []).concat(messengerShifts)"
-                class="flex flex-row pt-3 align-items-center w-100 border-bottom"
-                [shift]="shift"
-                (deleteFromCheckIn)="deleteFromCheckin($event)"
-              >
-              </shift>
-            </div>
-
-            <button mat-raised-button class="m-4 fex-button" (click)="checkIn()" matDialogClose>jetzt einchecken</button>
-          </div>
-
-          <div *ngIf="openShiftsToday.length" class="mb-3 border-top border-bottom shiftBlock">
-            <h3 class="my-3">eingecheckte kurier:innen</h3>
-            <div style="min-height: 100px">
-              <shift
-                *ngFor="let shift of openShiftsToday"
-                class="flex flex-row pt-3 align-items-center justify-content-between w-100 border-bottom"
-                [shift]="shift"
-                (contextmenu)="onRightClick($event, shift)"
-              >
-              </shift>
-            </div>
-          </div>
-
-          <div *ngIf="closedShiftsToday.length" class="border-top border-bottom shiftBlock">
-            <h3 class="my-3">beendete schichten</h3>
-            <div style="min-height: 100px">
-              <shift
-                *ngFor="let shift of closedShiftsToday"
-                class="flex flex-row pt-3 align-items-center justify-content-between w-100 border-bottom"
-                [shift]="shift"
-                (contextmenu)="onRightClick($event, shift)"
-              >
-              </shift>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      </mat-tab>
+    </mat-tab-group>
     <div class="container">
-      <div style="visibility: hidden; position: fixed" [style.left.px]="menuTopLeftPosition.x" [style.top.px]="menuTopLeftPosition.y" [matMenuTriggerFor]="rightMenu"></div>
+      <div
+        style="visibility: hidden; position: fixed"
+        [style.left.px]="menuTopLeftPosition.x"
+        [style.top.px]="menuTopLeftPosition.y"
+        [matMenuTriggerFor]="rightMenu"
+      ></div>
       <mat-menu #rightMenu="matMenu">
         <ng-template matMenuContent let-item="item">
           <right-click-menu [shift]="item"> </right-click-menu>
