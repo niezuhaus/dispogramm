3f97a7c8728ce5ee24fc987d462d6aea2ecffb2d
dani* <53942131+niezuhaus@users.noreply.github.com>
2025-09-11 16:51:47 +0200

    layout for material 15

--- src/app/dialogs/are-you-sure-dialog.component.ts
+++ src/app/dialogs/are-you-sure-dialog.component.ts
@@ -7,76 +7,78 @@ import { Messenger } from '../classes/Messenger';
 @Component({
   selector: 'app-are-you-sure-dialog',
   template: `
-    <div class="flex flex-row">
-      <div *ngIf="data.warning" class="mr-3 flex fex-warn">
-        <i class="bi bi-exclamation-triangle" style="font-size: 30px"></i>
+    <div class="p-4">
+      <div class="flex flex-row">
+        <div *ngIf="data.warning" class="mr-3 flex fex-warn">
+          <i class="bi bi-exclamation-triangle" style="font-size: 30px"></i>
+        </div>
+        <div>
+          <h1 mat-dialog-title [innerHTML]="data.headline" style="max-width: 450px"></h1>
+          <h6>{{ data.text }}</h6>
+        </div>
       </div>
-      <div>
-        <h1 mat-dialog-title [innerHTML]="data.headline" style="max-width: 450px"></h1>
-        <h6>{{ data.text }}</h6>
-      </div>
-    </div>
-    <description
-      class="mb-2 ml-3"
-      *ngIf="data.job"
-      [hideTimeAlarms]="true"
-      [job]="data.job"
-      [headline]="true"
-      [hideToolTips]="true"
-      [hideHighlights]="true"
-      matDialogClose
-      [hideHints]="true"
-    ></description>
-    <searchinput
-      *ngIf="data.messSearch"
-      (resetted)="messenger = null"
-      [keepMessName]="true"
-      [searchMessenger]="true"
-      (messengerSelected)="messenger = $event"
-      [label]="'kurier:in'"
-      class="mt-4"
-    ></searchinput>
-    <div class="mt-4 justify-content-around">
-      <div class="flex flex-row justify-content-around">
-        <div class="flex flex-row justify-content-between" style="width: 100%;">
-          <button
-            #yes
-            mat-raised-button
-            class="fex-button"
-            [class.fex-button-try]="!data.highlightNo && !data.warning"
-            [class.fex-button-warn]="data.warning"
-            [class.fex-button-abort]="data.highlightNo && !data.warning"
-            [class.transparent]="!data.verbYes"
-            (click)="confirm.emit(messenger || true)"
-            matDialogClose
-          >
-            {{ data.verbYes ? (messenger ? messenger?.nickname + ' festlegen' : data.verbYes) : 'ja' }}
-          </button>
-          <button
-            #no
-            mat-raised-button
-            [autofocus]="data.highlightNo"
-            class="fex-button"
-            [class.fex-button-try]="data.highlightNo"
-            [class.fex-button-abort]="!data.highlightNo"
-            [class.transparent]="!data.verbNo"
-            (click)="cancel.emit(true)"
-            matDialogClose
-            [type]="'submit'"
-          >
-            {{ data.verbNo ? data.verbNo : 'abbrechen' }}
-          </button>
+      <description
+        class="mb-2 ml-3"
+        *ngIf="data.job"
+        [hideTimeAlarms]="true"
+        [job]="data.job"
+        [headline]="true"
+        [hideToolTips]="true"
+        [hideHighlights]="true"
+        matDialogClose
+        [hideHints]="true"
+      ></description>
+      <searchinput
+        *ngIf="data.messSearch"
+        (resetted)="messenger = null"
+        [keepMessName]="true"
+        [searchMessenger]="true"
+        (messengerSelected)="messenger = $event"
+        [label]="'kurier:in'"
+        class="mt-4"
+      ></searchinput>
+      <div class="mt-4 justify-content-around">
+        <div class="flex flex-row justify-content-around">
+          <div class="flex flex-row justify-content-between" style="width: 100%;">
+            <button
+              #yes
+              mat-raised-button
+              class="fex-button"
+              [class.fex-button-try]="!data.highlightNo && !data.warning"
+              [class.fex-button-warn]="data.warning"
+              [class.fex-button-abort]="data.highlightNo && !data.warning"
+              [class.transparent]="!data.verbYes"
+              (click)="confirm.emit(messenger || true)"
+              matDialogClose
+            >
+              {{ data.verbYes ? (messenger ? messenger?.nickname + ' festlegen' : data.verbYes) : 'ja' }}
+            </button>
+            <button
+              #no
+              mat-raised-button
+              [autofocus]="data.highlightNo"
+              class="fex-button"
+              [class.fex-button-try]="data.highlightNo"
+              [class.fex-button-abort]="!data.highlightNo"
+              [class.transparent]="!data.verbNo"
+              (click)="cancel.emit(true)"
+              matDialogClose
+              [type]="'submit'"
+            >
+              {{ data.verbNo ? data.verbNo : 'abbrechen' }}
+            </button>
+          </div>
         </div>
       </div>
     </div>
   `,
   styles: [
     `
-          * {
-            display: flex;
-            flex-direction: column;
-          }
-        `
+      * {
+        display: flex;
+        flex-direction: column;
+      }
+    `
   ]
 })
 export class AreYouSureDialogComponent {
