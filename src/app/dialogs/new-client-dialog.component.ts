522462235ddf34b1c808395acd5f5d3c52788e89
dani* <53942131+niezuhaus@users.noreply.github.com>
2026-03-31 01:42:23 +0200

    refactor and sutofoc

--- src/app/dialogs/new-client-dialog.component.ts
+++ src/app/dialogs/new-client-dialog.component.ts
@@ -10,7 +10,7 @@ import { Client } from '../classes/Client';
     <div style="min-width: 400px">
       <h1 mat-dialog-title>neue kund:in erstellen</h1>
       <div class="m-auto" style="width: fit-content;">
-        <mat-button-toggle-group [value]="client.c.billClient" style="margin-bottom: 10px" class="flex flex-row">
+        <mat-button-toggle-group [value]="clientObject.c.billClient" style="margin-bottom: 10px" class="flex flex-row">
           <mat-button-toggle [value]="true" (click)="billClientChange(true)">rechnungskund:in</mat-button-toggle>
           <mat-button-toggle [value]="false" (click)="billClientChange(false)">barkund:in</mat-button-toggle>
         </mat-button-toggle-group>
@@ -18,34 +18,34 @@ import { Client } from '../classes/Client';
       <div mat-dialog-content>
         <mat-form-field>
           <mat-label>name</mat-label>
-          <input #name type="text" matInput [(ngModel)]="client.c.name" (keyup)="client.l.name = name.value" autofocus />
+          <input #name type="text" cdkFocusInitial matInput [(ngModel)]="clientObject.c.name" (keyup)="clientObject.l.name = name.value" />
         </mat-form-field>
         <mat-form-field>
           <mat-label>rufname</mat-label>
-          <input #nick type="text" matInput [value]="client.c.name" [(ngModel)]="client.l.name" (focus)="nick.select()" autofocus />
+          <input #nick type="text" matInput [value]="clientObject.c.name" [(ngModel)]="clientObject.l.name" (focus)="nick.select()" autofocus />
         </mat-form-field>
         <searchinput
           [label]="'straße'"
           [searchOSM]="true"
           (locationSelected)="addressSelected($event)"
-          (keyup)="client.c.street = search.searchTerm"
-          [(str)]="client.c.street"
+          (keyup)="clientObject.c.street = search.searchTerm"
+          [(str)]="clientObject.c.street"
           class="w-100"
-          (resetted)="client.c.zipCode = ''; client.c.city = ''"
+          (resetted)="clientObject.c.zipCode = ''; clientObject.c.city = ''"
           #search
         >
         </searchinput>
         <mat-form-field>
           <mat-label>postleitzahl</mat-label>
-          <input type="text" matInput [(ngModel)]="client.c.zipCode" />
+          <input type="text" matInput [(ngModel)]="clientObject.c.zipCode" />
         </mat-form-field>
         <mat-form-field>
           <mat-label>stadt</mat-label>
-          <input type="text" matInput [(ngModel)]="client.c.city" />
+          <input type="text" matInput [(ngModel)]="clientObject.c.city" />
         </mat-form-field>
         <mat-form-field>
           <mat-label>fred kund:innennummer</mat-label>
-          <input type="text" matInput [(ngModel)]="client.c.clientId" />
+          <input type="text" matInput [(ngModel)]="clientObject.c.clientId" />
         </mat-form-field>
         <button mat-raised-button class="ml-3 fex-button" (click)="this.save()" matDialogClose="true">speichern</button>
       </div>
@@ -53,16 +53,16 @@ import { Client } from '../classes/Client';
   `,
   styles: [
     `
-          * {
-            display: flex;
-            flex-direction: column;
-          }
-        `
+      * {
+        display: flex;
+        flex-direction: column;
+      }
+    `
   ]
 })
 export class NewClientDialogComponent {
   saved = new EventEmitter<{ c: Client; l: Geolocation }>();
-  client: { c: Client; l: Geolocation } = { c: new Client(), l: new Geolocation() };
+  clientObject: { c: Client; l: Geolocation } = { c: new Client(), l: null };
   nextClientIds: { netto: string; brutto: string };
 
   constructor(
@@ -71,53 +71,53 @@ export class NewClientDialogComponent {
       location: Geolocation;
     }
   ) {
-    this.client.c.billClient = true;
+    this.clientObject.c.billClient = true;
     if (data?.location) {
-      this.client.l = data.location;
-      this.client.c.street = data.location.street;
-      this.client.c.zipCode = data.location.zipCode;
-      this.client.c.city = data.location.city;
+      this.clientObject.l = data.location;
+      this.clientObject.c.street = data.location.street;
+      this.clientObject.c.zipCode = data.location.zipCode;
+      this.clientObject.c.city = data.location.city;
     }
     this.nextClientIds = GC.nextClientIds();
-    this.client.c.clientId = this.nextClientIds.netto;
+    this.clientObject.c.clientId = this.nextClientIds.netto;
   }
 
   save(): void {
-    GC.http.createClient(this.client.c).subscribe((client) => {
-      this.client.c = client;
-      if (Math.abs(this.client.l.latitude) > 0) {
-        this.client.l.clientId = client.id;
-        if (!this.client.l.id) {
-          GC.http.createLocation(this.client.l).subscribe((l) => {
+    GC.http.createClient(this.clientObject.c).subscribe((client) => {
+      this.clientObject.c = client;
+      if (this.clientObject.l) {
+        this.clientObject.l.clientId = client.id;
+        if (!this.clientObject.l.id) {
+          GC.http.createLocation(this.clientObject.l).subscribe((l) => {
             GC.openSnackBarLong(`kund:in und standort ${client.name} wurden gespeichert!`);
-            this.client.l = l;
-            this.saved.emit(this.client);
+            this.clientObject.l = l;
+            this.saved.emit(this.clientObject);
           });
         } else {
-          console.log(this.client.l);
-          GC.http.updateLocation(this.client.l).subscribe((l) => {
+          console.log(this.clientObject.l);
+          GC.http.updateLocation(this.clientObject.l).subscribe((l) => {
             GC.openSnackBarLong(`kund:in und standort ${client.name} wurden gespeichert!`);
-            this.client.l = l;
-            this.saved.emit(this.client);
+            this.clientObject.l = l;
+            this.saved.emit(this.clientObject);
           });
         }
       } else {
         GC.openSnackBarLong(`kund:in ${client.name} wurde gespeichert!`);
-        this.saved.emit(this.client);
+        this.saved.emit(this.clientObject);
       }
     });
   }
 
   billClientChange(isBillClient: boolean): void {
-    this.client.c.billClient = isBillClient;
-    this.client.c.clientId = isBillClient ? this.nextClientIds.netto : this.nextClientIds.brutto;
+    this.clientObject.c.billClient = isBillClient;
+    this.clientObject.c.clientId = isBillClient ? this.nextClientIds.netto : this.nextClientIds.brutto;
   }
 
   addressSelected(loc: Geolocation): void {
-    loc.name = this.client.l.name;
-    this.client.l = loc;
-    this.client.c.street = loc.street;
-    this.client.c.zipCode = loc.zipCode;
-    this.client.c.city = loc.city;
+    loc.name = this.clientObject.l.name;
+    this.clientObject.l = loc;
+    this.clientObject.c.street = loc.street;
+    this.clientObject.c.zipCode = loc.zipCode;
+    this.clientObject.c.city = loc.city;
   }
 }
