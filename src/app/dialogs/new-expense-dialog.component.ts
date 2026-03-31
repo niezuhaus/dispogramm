be004c3687df781725db148fbd8b7d57b4fe1501
dani* <53942131+niezuhaus@users.noreply.github.com>
2026-04-01 00:38:16 +0200

    expense dialog padding

--- src/app/dialogs/new-expense-dialog.component.ts
+++ src/app/dialogs/new-expense-dialog.component.ts
@@ -7,33 +7,37 @@ import { Expense } from '../classes/Expense';
 @Component({
   selector: 'app-new-expense-dialog',
   template: `
-    <h1 mat-dialog-title>{{ expense?.id ? 'auslage bearbeiten' : 'neue auslage hinzufügen' }}</h1>
+    <mat-tab-group dynamicHeight class="animated-width">
+      <mat-tab label="{{ expense?.id ? 'auslage bearbeiten' : 'neue auslage hinzufügen' }}">
+        <div class="p-4">
+          <!--    <div style="width: fit-content;">-->
+          <!--      <mat-button-toggle-group [(ngModel)]="isBrutto" style="margin-bottom: 10px" class="flex flex-row">-->
+          <!--        <mat-button-toggle [value]="false" (click)="isBrutto = false">netto</mat-button-toggle>-->
+          <!--        <mat-button-toggle [value]="true" (click)="isBrutto = true">brutto</mat-button-toggle>-->
+          <!--      </mat-button-toggle-group>-->
+          <!--    </div>-->
 
-    <!--    <div style="width: fit-content;">-->
-    <!--      <mat-button-toggle-group [(ngModel)]="isBrutto" style="margin-bottom: 10px" class="flex flex-row">-->
-    <!--        <mat-button-toggle [value]="false" (click)="isBrutto = false">netto</mat-button-toggle>-->
-    <!--        <mat-button-toggle [value]="true" (click)="isBrutto = true">brutto</mat-button-toggle>-->
-    <!--      </mat-button-toggle-group>-->
-    <!--    </div>-->
+          <div class="flex flex-column">
+            <div class="flex flex-row">
+              <mat-form-field class="mr-4" style="max-width: 200px">
+                <mat-label>beschreibung</mat-label>
+                <input #search matInput [(ngModel)]="expense.description" />
+              </mat-form-field>
 
-    <div class="flex flex-column">
-      <div class="flex flex-row">
-        <mat-form-field class="mr-4" style="max-width: 200px">
-          <mat-label>beschreibung</mat-label>
-          <input #search matInput [(ngModel)]="expense.description" />
-        </mat-form-field>
+              <mat-form-field style="width: 75px">
+                <mat-label>euro</mat-label>
+                <input #value matInput type="number" (click)="value.select()" [(ngModel)]="expense.price._brutto" autofocus />
+              </mat-form-field>
+            </div>
 
-        <mat-form-field style="width: 75px">
-          <mat-label>euro</mat-label>
-          <input #value matInput type="number" (click)="value.select()" [(ngModel)]="expense.price._brutto" autofocus />
-        </mat-form-field>
-      </div>
-
-      <div class="w-100 flex flex-row justify-between">
-        <button mat-raised-button class="fex-button" (click)="save()" mat-dialog-close>speichern</button>
-        <button mat-raised-button class="fex-button-warn" [class.hidden]="!expense.id" (click)="delete()" mat-dialog-close>löschen</button>
-      </div>
-    </div>
+            <div class="w-100 flex flex-row justify-between">
+              <button mat-raised-button class="fex-button" (click)="save()" mat-dialog-close>speichern</button>
+              <button mat-raised-button class="fex-button-warn" [class.hidden]="!expense.id" (click)="delete()" mat-dialog-close>löschen</button>
+            </div>
+          </div>
+        </div>
+      </mat-tab>
+    </mat-tab-group>
   `,
   styles: []
 })
