b505e8660eb434c6ac4207847000776fa93834a0
dani* <53942131+niezuhaus@users.noreply.github.com>
2026-04-07 05:00:16 +0200

    no next working days in tourplan

--- src/app/views/tourplan/tourplan.component.ts
+++ src/app/views/tourplan/tourplan.component.ts
@@ -185,7 +185,7 @@ export class TourplanComponent extends TitleComponent implements OnInit, AfterVi
       this.loaded = true;
       this.route.paramMap.pipe(takeUntil(this.destroy$)).subscribe((params) => {
         if (!params.get('date') || GC.openedLast != new Date().yyyymmdd()) {
-          this.date = new Date().nextWorkingDay();
+          // this.date = new Date().nextWorkingDay();
           this.location.replaceState(`${GC.routes.tourplan}`, `date=${this.date.yyyymmdd()}`);
           GC.openedLast = new Date().yyyymmdd();
           setItem('openedLast', GC.openedLast);
